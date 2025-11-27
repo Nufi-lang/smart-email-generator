d2b19316198182e20528333b4247f78bd542e13b
Nufi-lang <nufaizahallaj@gmail.com>
2025-11-27 20:09:01 +0530

    smart email generator backend before chrome adding chrome extension

---
+++ src/main/java/com/email/writer/WriterSbApplication.java
@@ -0,0 +1,14 @@
+package com.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.
+				run(WriterSbApplication.class, args);
+	}
+
+}
